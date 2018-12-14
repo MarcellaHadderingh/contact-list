cddc6bbe43fd9fd138084adfc8e01fdd6d4737e0
marcellaHadderingh <marcellavanduyn@gmail.com>
2018-12-14 12:50:36 +0100

    index main and style
    index orderd, main nothing, style css

--- main.js
+++ main.js
@@ -1,61 +1,59 @@
-function voegIemandToe()
-    {
-        var node = document.createElement("LI"); 
-        var sNaam = document.getElementById("myNewName").value;
-var textnode = document.createTextNode(sNaam);         
-node.appendChild(textnode);                             
-document.getElementById("myList").appendChild(node);     
-    }
+function voegIemandToe() {
+    var node = document.createElement("LI");
+    var sNaam = document.getElementById("myNewName").value;
+    var textnode = document.createTextNode(sNaam);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+}
 
-    function myFilterFunction(){
+function myFilterFunction() {
     var sSearchWord = document.getElementById("myFilterBox").value;
     console.log(sSearchWord);
 }
-    
-function myFilterFunction()
-    {
-         var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById("myFilterBox");
-        filter = input.value.toUpperCase();
-        
-        ul = document.getElementById("myList");
-        li = ul.getElementsByTagName('li');
-  
-        for (i = 0; i < li.length; i++) { 
+
+function myFilterFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myFilterBox");
+    filter = input.value.toUpperCase();
+
+    ul = document.getElementById("myList");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
         txtValue = li[i].innerHTML;
-        
-         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
         }
+    }
 }
 //sort function
 function sortList() {
-  var list, i, switching, b, shouldSwitch;
-  list = document.getElementById("myList");
-  switching = true;
-  
-  while (switching) {
-    
-    switching = false;
-    b = list.getElementsByTagName("LI");
-   
-    for (i = 0; i < (b.length - 1); i++) {
-    
-      shouldSwitch = false;
-  
-      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-       
-        shouldSwitch = true;
-        break;
-      }
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("myList");
+    switching = true;
+
+    while (switching) {
+
+        switching = false;
+        b = list.getElementsByTagName("LI");
+
+        for (i = 0; i < (b.length - 1); i++) {
+
+            shouldSwitch = false;
+
+            if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+        }
     }
-    if (shouldSwitch) {
-      
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-    }
-  }
 }
